ae1a472b3cfde68b901bae3f7516d8555a5527f3
Cristina Quijano <cristina@cristinas-air-2.kpn>
2022-04-06 21:38:23 +0200

    app.js

--- app.js
+++ app.js
@@ -12,14 +12,21 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beers"), (req,res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi =>{
+res.render('beers', {beersFromApi});
+  })
+
+app.get("/random-beer", (req,res) => {
+  punkAPI
+  .getRandom()
+    .then(responseFromAPI => res.render('random-beer', {responseFromAPI}))
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
